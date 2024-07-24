b2a625350d0cefc72a7eee643a21e88afc46b13a
Promin <3056063115@qq.com>
2024-07-25 07:57:02 +0800

    cow finished

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,9 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+int cowpage(pagetable_t , uint64 );
+void* cowalloc(pagetable_t , uint64 ) ;
+int kaddrefcnt(void* );
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -170,6 +173,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+pte_t *         walk(pagetable_t , uint64 , int );
 
 // plic.c
 void            plicinit(void);
